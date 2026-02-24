1ec5ad457c272c7fbd9018232e0cebe2cba86e3d
arthur <arthur.a.martins00@gmail.com>
2026-02-24 16:57:24 -0300

    RegisterScreenComplete_Maybe

---
+++ app/src/main/java/com/example/quizapp/ui/screens/auth/AuthScreensEvent.kt
@@ -0,0 +1,4 @@
+package com.example.quizapp.ui.screens.auth
+
+interface AuthScreensEvent {
+}
--- app/src/main/java/com/example/quizapp/ui/screens/auth/login/LoginScreenEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quizapp.ui.screens.auth.login
-
-
-interface LoginScreenEvent {
-    data class onEmailChange(val email: String): LoginScreenEvent
-    data class onPasswordChange(val password: String): LoginScreenEvent
-    data object onSignIn: LoginScreenEvent
-    data object onSignUpClick: LoginScreenEvent
-}
